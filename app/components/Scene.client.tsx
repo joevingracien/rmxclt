5349324e0faba3a5aed635dc5bb94cd2ccbdfd78
joBar <jg.getintouch@gmail.com>
2024-02-05 18:49:33 +0100

    remixclient

---
+++ app/components/Scene.client.tsx
@@ -0,0 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+
+export default function Scene() {
+  return (
+    <div>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
+}
